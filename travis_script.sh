234afd08c9de37a9047413fcc87ba99568ef34c6
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-09-23 08:09:55 +0000

    Add travis ci test with sge master and client

--- travis_script.sh
+++ travis_script.sh
@@ -1,3 +1,22 @@
-#!/bin/bash
+docker run --hostname sgemaster --name sgemaster -d -v $PWD/master_script.sh:/usr/local/bin/master_script.sh  manabuishii/docker-sge-master:0.1.0 /usr/local/bin/master_script.sh
+docker run -d -ti --hostname sgeclient --name sgeclient --link sgemaster:sgemaster -v $PWD/act_qmaster:/var/lib/gridengine/default/common/act_qmaster -v $PWD/work:/work --entrypoint=/bin/bash manabuishii/docker-sge-client:0.1.0
+# wait to sge master
+sleep 10
 
-exit 1
+SGECLIENT=$(docker exec sgeclient cat /etc/hosts | grep sgeclient)
+docker exec sgemaster bash -c "echo ${SGECLIENT} >> /etc/hosts"
+docker exec sgemaster qconf -as sgeclient
+
+docker exec sgeclient bash -c "echo 'hostname ; date' | qsub -o /tmp/a.txt"
+# wait to finish job
+sleep 10
+docker exec sgemaster cat /tmp/a.txt | grep sgemaster
+RET=$?
+echo "RET=[${RET}]"
+
+docker stop sgemaster
+docker rm sgemaster
+docker stop sgeclient
+docker rm sgeclient
+
+exit ${RET}
